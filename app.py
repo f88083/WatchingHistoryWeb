dbd12ec1cf479340bf917fa1cad13cf2c5ba37b5
f88083 <f88083@gmail.com>
2023-11-13 21:38:06 +0000

    Use Json file to store history

--- app.py
+++ app.py
@@ -4,15 +4,25 @@ import json
 # Init. the Flask app
 app = Flask(__name__)
 
-# List to store the viewing history
-viewing_history = []
+# Path to the JSON file
+JSON_FILE_PATH = 'data/watching_history.json'
 
-# Route to display the viewing history
+# Load initial watching history from the JSON file if it exists
+def load_watching_history():
+    try:
+        with open(JSON_FILE_PATH, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+watching_history = load_watching_history()
+
+# Route to display the watching history
 @app.route('/')
 def index():
-    return render_template('index.html', history=viewing_history)
+    return render_template('index.html', history=watching_history)
 
-# Route to add a new entry to the viewing history
+# Route to add a new entry to the watching history
 @app.route('/add', methods=['POST'])
 def add():
     title = request.form['title']
@@ -20,9 +30,13 @@ def add():
     progress = int(request.form['progress'])
 
     new_entry = {'title': title, 'category': category, 'progress': progress}
-    viewing_history.append(new_entry)
+    watching_history.append(new_entry)
 
-    return jsonify({'success': True, 'history': viewing_history})
+    # Save updated watching history to the JSON file
+    with open(JSON_FILE_PATH, 'w') as file:
+        json.dump(watching_history, file, indent=2)
+
+    return jsonify({'success': True, 'history': watching_history})
 
 if __name__ == '__main__':
     app.run(debug=True)
