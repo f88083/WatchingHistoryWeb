9a8b0523edd03cbafb64f25da270ca21a621c26e
f88083 <f88083@gmail.com>
2023-11-13 15:44:24 +0000

    Init. commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+viewing_history = []
+
+# Route to display the viewing history
+@app.route('/')
+def index():
+    return render_template('index.html', history=viewing_history)
+
+# Route to add a new entry to the viewing history
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form['title']
+    category = request.form['category']
+    progress = int(request.form['progress'])
+
+    new_entry = {'title': title, 'category': category, 'progress': progress}
+    viewing_history.append(new_entry)
+
+    return render_template('index.html', history=viewing_history)
+
+if __name__ == '__main__':
+    app.run(debug=True)
