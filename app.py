fbd98253c2054a336623179d0de8f12f66ae8138
f88083 <f88083@gmail.com>
2023-11-30 16:20:36 +0000

    Feat: Added database model and configured

--- app.py
+++ app.py
@@ -4,8 +4,23 @@ from flask_sqlalchemy import SQLAlchemy
 # Init. the Flask app
 app = Flask(__name__)
 
-# TODO: database
+# Config. database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///watching-history.db'
+# Init. database
+db = SQLAlchemy(app)
 
+# Create a model for database
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    season = db.Column(db.String(10), nullable=False)
+    value = db.Column(db.Integer, nullable=False)
+    episode = db.Column(db.Integer, nullable=False)
+    progress = db.Column(db.String(10), nullable=False)
+
+    # Return a string when create a new element
+    def __repr__(self):
+        return '<Watching History %r>' % self.id
 
 # Route to display the watching history
 @app.route("/")
