0c049e44f14513a811cf04cdec3855fa7265c57b
f88083 <f88083@gmail.com>
2023-11-30 16:04:56 +0000

    Fix: Abandoned GPT codes

--- app.py
+++ app.py
@@ -1,59 +1,22 @@
 from flask import Flask, render_template, request, jsonify
-import json
+from flask_sqlalchemy import SQLAlchemy
 
 # Init. the Flask app
 app = Flask(__name__)
 
-# Path to the JSON file
-JSON_FILE_PATH = 'data/watching_history.json'
+# TODO: database
 
-# Load initial watching history from the JSON file if it exists
-def load_watching_history():
-    try:
-        with open(JSON_FILE_PATH, 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []
-
-watching_history = load_watching_history()
 
 # Route to display the watching history
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', history=watching_history)
+    return render_template("index.html")
+
 
 # Route to add a new entry to the watching history
-@app.route('/add', methods=['POST'])
+@app.route("/add", methods=["POST"])
 def add():
-    title = request.form['title']
-    category = request.form['category']
-    progress = int(request.form['progress'])
+    pass
 
-    new_entry = {'title': title, 'category': category, 'progress': progress}
-    watching_history.append(new_entry)
-
-    # Save updated watching history to the JSON file
-    with open(JSON_FILE_PATH, 'w') as file:
-        json.dump(watching_history, file, indent=2)
-
-    return jsonify({'success': True, 'history': watching_history})
-
-# Route to delete an entry from the watching history
-@app.route('/delete', methods=['POST'])
-def delete():
-    index = int(request.form['index'])
-    
-    print(watching_history[index])
-    
-    # Remove the entry from the watching history
-    del watching_history[index]
-
-    # Save updated watching history to the JSON file
-    with open(JSON_FILE_PATH, 'w') as file:
-        json.dump(watching_history, file, indent=2)
-
-    return jsonify({'success': True, 'history': watching_history})
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
